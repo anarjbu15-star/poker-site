24e2438dc7403903020e0eaaf36a55ea8d5ccf9f
anarjbu15-star <anarjbu15@gmail.com>
2025-11-27 00:59:36 +0100

    Update game.js

--- public/game.js
+++ public/game.js
@@ -4,64 +4,39 @@ let timerInterval;
 
 function login() {
   username = document.getElementById("username").value.trim();
-  if (!username) return alert("Enter a name!");
+  if (!username) return alert("Enter a username!");
 
   document.getElementById("login-screen").classList.add("hidden");
-  document.getElementById("lobby-screen").classList.remove("hidden");
-  document.getElementById("player-name").innerText = username;
-}
-
-function joinTable() {
-  document.getElementById("lobby-screen").classList.add("hidden");
   document.getElementById("table-screen").classList.remove("hidden");
 
-  // Render needs wss:// + host
-  const wsURL = (location.protocol === "https:" ? "wss://" : "ws://") + location.host;
-  ws = new WebSocket(wsURL);
+  connectWS();
+}
+
+function connectWS() {
+  ws = new WebSocket("wss://" + window.location.host);
 
   ws.onopen = () => {
     ws.send(JSON.stringify({ type: "join", name: username }));
   };
 
-  ws.onmessage = (msg) => {
+  ws.onmessage = msg => {
     const data = JSON.parse(msg.data);
 
-    if (data.type === "cards") {
+    if (data.type === "communityCards") {
       updateCommunityCards(data.cards);
-    }
-
-    if (data.type === "hole") {
-      updateHoleCards(data.cards);
-    }
-
-    if (data.type === "timer") {
-      startTimer(data.seconds);
+      startTimer(7);
     }
   };
 }
 
-// Show community cards
 function updateCommunityCards(cards) {
   const area = document.getElementById("community-cards");
   area.innerHTML = "";
 
   cards.forEach(card => {
-    let img = document.createElement("img");
-    img.src = `https://deckofcardsapi.com/static/img/${card}.png`;
+    const img = document.createElement("img");
     img.className = "card-img";
-    area.appendChild(img);
-  });
-}
-
-// Show player hand (your two cards)
-function updateHoleCards(cards) {
-  const area = document.getElementById("player-cards");
-  area.innerHTML = "";
-
-  cards.forEach(card => {
-    let img = document.createElement("img");
     img.src = `https://deckofcardsapi.com/static/img/${card}.png`;
-    img.className = "card-img";
     area.appendChild(img);
   });
 }
@@ -72,15 +47,15 @@ function sendAction(action) {
 
 function startTimer(seconds) {
   clearInterval(timerInterval);
-  let time = seconds;
+  let t = seconds;
 
-  document.getElementById("timer").innerText = time;
+  document.getElementById("timer").innerText = t;
 
   timerInterval = setInterval(() => {
-    time--;
-    document.getElementById("timer").innerText = time;
+    t--;
+    document.getElementById("timer").innerText = t;
 
-    if (time <= 0) {
+    if (t <= 0) {
       clearInterval(timerInterval);
       sendAction("fold");
     }
