6488cc8ef0953fdae65dd343ab26c386fa1a699a
anarjbu15-star <anarjbu15@gmail.com>
2025-11-26 22:36:07 +0100

    Create game.js

---
+++ public/game.js
@@ -0,0 +1,64 @@
+let ws;
+let username = "";
+let timerInterval;
+
+function login() {
+  username = document.getElementById("username").value.trim();
+  if (!username) return alert("Enter a name!");
+
+  document.getElementById("login-screen").classList.add("hidden");
+  document.getElementById("lobby-screen").classList.remove("hidden");
+  document.getElementById("player-name").innerText = username;
+}
+
+function joinTable() {
+  document.getElementById("lobby-screen").classList.add("hidden");
+  document.getElementById("table-screen").classList.remove("hidden");
+
+  ws = new WebSocket("wss://" + window.location.host);
+
+  ws.onopen = () => {
+    ws.send(JSON.stringify({ type: "join", name: username }));
+  };
+
+  ws.onmessage = (msg) => {
+    const data = JSON.parse(msg.data);
+
+    if (data.type === "cards") {
+      updateCommunityCards(data.cards);
+      startTimer(7);
+    }
+  };
+}
+
+function updateCommunityCards(cards) {
+  const area = document.getElementById("community-cards");
+  area.innerHTML = "";
+
+  cards.forEach(card => {
+    let img = document.createElement("img");
+    img.src = `https://deckofcardsapi.com/static/img/${card}.png`;
+    area.appendChild(img);
+  });
+}
+
+function sendAction(action) {
+  ws.send(JSON.stringify({ type: "action", action }));
+}
+
+function startTimer(seconds) {
+  clearInterval(timerInterval);
+  let time = seconds;
+
+  document.getElementById("timer").innerText = time;
+
+  timerInterval = setInterval(() => {
+    time--;
+    document.getElementById("timer").innerText = time;
+
+    if (time <= 0) {
+      clearInterval(timerInterval);
+      sendAction("fold");
+    }
+  }, 1000);
+}
